95bad41cf056c769c6e9906c65e7c866951372e9
MrPerry365 <1987nowhere@gmail.com>
2022-03-27 12:20:21 -0400

    add home page and style

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// 
+
+//
+
+// make arrays for 5 questions and answers 
+
+// make a function to check if question is correct. if so go to next question. if not deduct time and then go to next question
+
+// make 60 second countdown timer with stop function when all questions are answered or out of time
+
+// make function to replace high score with input text if true. if false prompt try again button
+
+//
